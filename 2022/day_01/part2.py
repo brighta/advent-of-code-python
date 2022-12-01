fbc9b93b5036efe2e55c9a785f4485bdb81ff34c
Alex Bright <alexander.bright@johnlewis.co.uk>
2022-12-01 08:41:29 +0000

    Added code for Day 1.

---
+++ 2022/day_01/part2.py
@@ -0,0 +1,32 @@
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit("Please enter the file name for the input file.")
+
+fileName = sys.argv[1]
+
+def loadInput(fileName):
+    with open(fileName) as file:
+        return file.readlines()
+
+def splitFileIntoElves(file):
+    elves = []
+    currentElf = 0
+    for line in file:
+        line = line.rstrip()
+        if line == "":
+            elves.append(currentElf)
+            currentElf = 0
+        else:
+            currentElf += int(line)
+    elves.append(currentElf)
+    return elves
+
+def findTopElvesSum(elves, number):
+    elves.sort(reverse=True)
+    return sum(elves[0:number])
+
+file = loadInput(fileName);
+elves = splitFileIntoElves(file)
+topElvesSum = findTopElvesSum(elves, 3)
+print(topElvesSum)
