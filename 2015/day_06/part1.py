aae1f9d438c846089949fa6061be38337b0fd727
Alex Bright <alexander.bright@johnlewis.co.uk>
2022-12-02 14:58:20 +0000

    Added code for 2015 Day 6.

---
+++ 2015/day_06/part1.py
@@ -0,0 +1,40 @@
+import sys
+import re
+
+if len(sys.argv) != 2:
+    sys.exit("Please enter the file name for the input file.")
+
+fileName = sys.argv[1]
+
+def loadInput(fileName):
+    with open(fileName) as file:
+        return file.readlines()
+
+def createGrid():
+    return [[0 for x in range(1000)] for x in range(1000)]
+
+def performActionOnGrid(action, grid):
+    actionParts = action.split(" ")
+    x1 = int(actionParts[-3].split(",")[0])
+    y1 = int(actionParts[-3].split(",")[1])
+    x2 = int(actionParts[-1].split(",")[0])
+    y2 = int(actionParts[-1].split(",")[1])
+    for x in range(x1, x2+1):
+        for y in range(y1, y2+1):
+            if actionParts[0] == "toggle":
+                grid[x][y] = abs(grid[x][y]-1)
+            elif actionParts[0] == "turn":
+                if actionParts[1] == "on":
+                    grid[x][y] = 1
+                elif actionParts[1] == "off":
+                    grid[x][y] = 0
+    return grid
+
+def countLightsOn(grid):
+    return sum([sum(i) for i in zip(*grid)])
+
+file = loadInput(fileName)
+grid = createGrid()
+for row in file:
+    grid = performActionOnGrid(row.rstrip(), grid)
+print(countLightsOn(grid))
---
+++ 2015/day_06/part2.py
@@ -0,0 +1,44 @@
+import sys
+import re
+
+if len(sys.argv) != 2:
+    sys.exit("Please enter the file name for the input file.")
+
+fileName = sys.argv[1]
+
+def loadInput(fileName):
+    with open(fileName) as file:
+        return file.readlines()
+
+def createGrid():
+    return [[0 for x in range(1000)] for x in range(1000)]
+
+def performActionOnGrid(action, grid):
+    actionParts = action.split(" ")
+    x1 = int(actionParts[-3].split(",")[0])
+    y1 = int(actionParts[-3].split(",")[1])
+    x2 = int(actionParts[-1].split(",")[0])
+    y2 = int(actionParts[-1].split(",")[1])
+    for x in range(x1, x2+1):
+        for y in range(y1, y2+1):
+            change = 0
+            if actionParts[0] == "toggle":
+                change = 2
+            elif actionParts[0] == "turn":
+                if actionParts[1] == "on":
+                    change = 1
+                elif actionParts[1] == "off":
+                    change = -1
+            grid[x][y] = grid[x][y] + change
+            if grid[x][y] < 0:
+                grid[x][y] = 0
+    return grid
+
+def countLightsOn(grid):
+    return sum([sum(i) for i in zip(*grid)])
+
+file = loadInput(fileName)
+grid = createGrid()
+for row in file:
+    grid = performActionOnGrid(row.rstrip(), grid)
+print(countLightsOn(grid))
