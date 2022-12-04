e75aed199315195652956de97796ddd0e556e1de
Alex Bright <alexander.bright@johnlewis.co.uk>
2022-12-04 10:02:09 +0000

    Added code for 2022 Day 4.

---
+++ 2022/day_04/part2.py
@@ -0,0 +1,22 @@
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit("Please enter the file name for the input file.")
+
+fileName = sys.argv[1]
+
+def loadInput(fileName):
+    with open(fileName) as file:
+        return file.readlines()
+
+def checkForAnyOverlap(firstStart, firstEnd, secondStart, secondEnd):
+    if firstStart <= secondStart <= firstEnd or firstStart <= secondEnd <= firstEnd or secondStart <= firstStart <= secondEnd or secondStart <= firstEnd <= secondStart:
+        return True
+
+file = loadInput(fileName)
+count = 0
+for row in file:
+    elfParts = row.rstrip().split(",")
+    if checkForAnyOverlap(int(elfParts[0].split("-")[0]), int(elfParts[0].split("-")[1]), int(elfParts[1].split("-")[0]), int(elfParts[1].split("-")[1])):
+        count += 1
+print(count)
