066e70f895fa9fcc2aaf2002b065d7f882a251d1
Alex Bright <alexander.bright@johnlewis.co.uk>
2023-12-13 08:10:19 +0000

    Adding day 12 for 2023.

---
+++ 2023/day_12/part2.py
@@ -0,0 +1,70 @@
+import sys
+import re
+import time
+
+if len(sys.argv) != 2:
+    sys.exit("Please enter the file name for the input file.")
+
+fileName = sys.argv[1]
+
+def loadInput(fileName):
+    with open(fileName) as file:
+        return file.read().splitlines()
+
+def getPuzzleInput(fileLines):
+    inputs = []
+    for fileLine in fileLines:
+        conditions = re.findall('[#?]+', '?'.join([fileLine.split(' ')[0]] * 5))
+        groups = list(map(int, re.findall('[0-9]+', ','.join([fileLine.split(' ')[1]] * 5))))
+        inputs.append([conditions, groups])
+    return inputs
+
+def checkIfSolutionsMatch(solution, test):
+    for i in range(len(test)):
+        if test[i] != solution[i] and solution[i] != '?':
+            return False
+    return True
+
+def checkCombinations(solution, test, difference, groups):
+    answer = 0
+    for i in range(difference + 1):
+        newTest = test + '.' * i + '#' * groups[0]
+        if checkIfSolutionsMatch(solution, newTest):
+            differenceRemaining = difference - i
+            if len(groups) == 1:
+                newTest += '.' * differenceRemaining
+                answer += 1 if checkIfSolutionsMatch(solution, newTest) else 0
+            else:
+                newTest += '.'
+                answer += checkCombinations(solution, newTest, differenceRemaining, groups[1:])
+    return answer
+
+def getNumberCombinations(input):
+    if len(input[0]) == 0 or len(input[1]) == 0:
+        return 1
+    if len('.'.join(input[0])) == sum(input[1]) + len(input[1]) - 1:
+        return 1
+    if len(input[0]) == 1 and len(list(set(input[0][0]))) == 1 and input[0][0][0] == '?' and len(input[1]) == 1:
+        return len(input[0][0]) - input[1][0] + 1
+    solution = '.'.join(input[0])
+    difference = len(solution) - sum(input[1]) - len(input[1]) + 1
+    answer = checkCombinations(solution, '', difference, input[1])
+    return answer
+
+count = 0
+def printInputAnswer(input, answer):
+    global count
+    count += 1
+    print(str(time.strftime("%Y-%m-%d %H:%M:%S")) + ' ' + str(count).rjust(4, ' ') + ' ' + '.'.join(input[0]).ljust(20, ' ') + ' - ' + ','.join(list(map(str, input[1]))).ljust(15, ' ') + ' - ' + str(answer))
+    return answer
+
+
+print(str(time.strftime("%Y-%m-%d %H:%M:%S")))
+fileLines = loadInput(fileName)
+inputs = getPuzzleInput(fileLines)
+answer = 0
+for input in inputs:
+    number = getNumberCombinations(input)
+    answer += number
+    printInputAnswer(input, number)
+print(answer)
