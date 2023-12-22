3f22fba0b0abdecfde18a08c731af71a40b242ea
Alex Bright <alexander.bright@johnlewis.co.uk>
2023-12-22 17:07:15 +0000

    Adding day 13 for 2015.

---
+++ 2015/day_13/part2.py
@@ -0,0 +1,47 @@
+import sys
+import itertools
+
+if len(sys.argv) != 2:
+    sys.exit("Please enter the file name for the input file.")
+
+fileName = sys.argv[1]
+
+def loadInput(fileName):
+    with open(fileName) as file:
+        return file.read().splitlines()
+
+def getPuzzleInput(fileLines):
+    people = ['me']
+    happinesses = {'me': {}}
+    for line in fileLines:
+        person1 = line.split(' ')[0]
+        person2 = line.split(' ')[-1][:-1]
+        happiness = int(line.split(' ')[3]) * (-1 if line.split(' ')[2] == 'lose' else 1)
+        if person1 in people:
+            happinesses[person1][person2] = happiness
+            happinesses[person1]['me'] = 0
+            happinesses['me'][person1] = 0
+        else:
+            people.append(person1)
+            happinesses[person1] = {person2: happiness}
+    return people, happinesses
+
+def getAnswer(people, happinesses):
+    maxAnswer = 0
+    for path in list(itertools.permutations(people)):
+        answer = 0
+        current = path[0]
+        for next in path[1:]:
+            answer += happinesses[current][next]
+            answer += happinesses[next][current]
+            current = next
+        answer += happinesses[path[-1]][path[0]]
+        answer += happinesses[path[0]][path[-1]]
+        maxAnswer = max(maxAnswer, answer)
+    return maxAnswer
+
+
+fileLines = loadInput(fileName)
+people, happinesses = getPuzzleInput(fileLines)
+answer = getAnswer(people, happinesses)
+print(answer)
