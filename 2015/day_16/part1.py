770ae8b9cbfca97d277eac059e9665ff2e46a8cf
Alex Bright <alexander.bright@johnlewis.co.uk>
2023-12-24 19:05:21 +0000

    Adding day 16 for 2015.

---
+++ 2015/day_16/part1.py
@@ -0,0 +1,40 @@
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit("Please enter the file name for the input file.")
+
+fileName = sys.argv[1]
+
+def loadInput(fileName):
+    with open(fileName) as file:
+        return file.read().splitlines()
+
+def stringToMap(string):
+    string = string.replace(':', ',')
+    stringParts = string.split(', ')
+    return dict(zip(stringParts[::2], list(map(int, stringParts[1::2]))))
+
+def getPuzzleInput(fileLines):
+    measurements = stringToMap(fileLines[0])
+    sues = []
+    for fileLine in fileLines[1:]:
+        valuesString = ' '.join(fileLine.split(' ')[2:])
+        sues.append(stringToMap(valuesString))
+    return measurements, sues
+
+def getAnswer(measurements, sues):
+    for i in range(len(sues)):
+        sue = sues[i]
+        correct = True
+        for property in sue:
+            if measurements[property] != sue[property]:
+                correct = False
+        if correct:
+            return i + 1
+    return 0
+
+
+fileLines = loadInput(fileName)
+measurements, sues = getPuzzleInput(fileLines)
+answer = getAnswer(measurements, sues)
+print(answer)
